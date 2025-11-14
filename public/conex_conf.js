9f7b309e25cc142aacd88d64719af740c546b6b8
guerratron <dinertron@gmail.com>
2025-11-14 19:15:38 +0100

    Routes
    Pequeña corrección de algunas rutas y algún cambio menor

--- public/conex_conf.js
+++ public/conex_conf.js
@@ -3,3 +3,4 @@ export const protocol = "http";
 export const host = "localhost";
 export const port = "8080"; //"8181"; //"5500";
 export const path = "AppWeb-CRUD/src/"; //"www/PRUEBAS/EXPERIMENTOS/AppWeb-CRUD/src/";
+//https://appcrud.kesug.com/src/clients.php?action=list&
